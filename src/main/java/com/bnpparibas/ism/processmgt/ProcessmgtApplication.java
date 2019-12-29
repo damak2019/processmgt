32ce93503c870cfb0f039b181edebd50b2856593
mohameddamak <mohamed2019.damak@gmail.com>
2019-12-29 15:56:13 +0100

    initialising data in mvp1

--- src/main/java/com/bnpparibas/ism/processmgt/ProcessmgtApplication.java
+++ src/main/java/com/bnpparibas/ism/processmgt/ProcessmgtApplication.java
@@ -2,13 +2,19 @@ package com.bnpparibas.ism.processmgt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ProcessmgtApplication {
+public class ProcessmgtApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {		SpringApplication.run(ProcessmgtApplication.class, args);
 
 		System.out.println("Hello Process MGT");
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("PUT", "DELETE","POST", "GET");
+	}
 }
